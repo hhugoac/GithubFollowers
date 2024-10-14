baecbf8cb0e9dba1d3670a74a4bd87f7a0ae340c
Hugo Alonzo <hhugoac@github.com>
2024-10-13 22:30:46 -0600

    Initial Commit

---
+++ github/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  github
+//
+//  Created by Hugo Alonzo on 13/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ githubTests/githubTests.swift
@@ -0,0 +1,17 @@
+//
+//  githubTests.swift
+//  githubTests
+//
+//  Created by Hugo Alonzo on 13/10/24.
+//
+
+import Testing
+@testable import github
+
+struct githubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
