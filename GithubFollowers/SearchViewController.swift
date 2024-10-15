ebe7b7a4f3b234f511b8b32a13754c2f90656070
hectoralonzofg <hector_alonzo@fragua.com.mx>
2024-10-14 18:17:34 -0600

    SEARCH VIEWCONTROLLER ADN CUSTOM ALERT

--- GithubFollowers/SearchViewController.swift
+++ GithubFollowers/SearchViewController.swift
@@ -39,7 +39,7 @@ class SearchViewController: UIViewController {
     @objc
     func pushToFollowersViewController() {
         guard isUsernameEntered else {
-            print("🚩🚩🚩 No username")
+            presentGFAlertOnMainThread(title: "Empty Username", message: "Please enter a username. We need to know who to look for 🤠.", buttonTitle: "OK")
             return
         }
         
